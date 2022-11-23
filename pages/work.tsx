4035a66857fd666c13c69c0d35195c2cffef3d8c
Ian Roberts <ian.roberts@designaroni.com>
2022-11-23 13:19:14 -0700

    Adds or reformats about and work page content, adds LineBreak component, adds outbound link support to MotionLinkInline

--- pages/work.tsx
+++ pages/work.tsx
@@ -8,18 +8,15 @@ import {
   TopLevelPageNames,
 } from "@/lib/api";
 import PageHead from "@/components/general/pageHead";
-// import styles from "@/styles/pages/work.module.scss";
 import Section from "@/components/section/section";
 import MotionFadeInWhenInView from "@/components/motionFadeInWhenInView/motionFadeInWhenInView";
-import MotionFadeAndStaggerChildrenWhenInView from "@/components/motionFadeAndStaggerChildrenWhenInView/motionFadeAndStaggerChildrenWhenInView";
-import FutureImage from "next/future/image";
 import Row from "@/components/row/row";
 import Tab from "@/components/tab/tab";
 import Title from "@/components/title/title";
 import Column from "@/components/column/column";
-import Markdown from "markdown-to-jsx";
-import { Fragment } from "react";
 import styles from "@/styles/pages/about.module.scss";
+import LineBreak from "@/components/linebreak/linebreak";
+import MotionLinkInline from "@/components/general/motionLinkInline";
 
 interface PageProps {
   footerData: FooterData;
@@ -31,102 +28,10 @@ const Work: NextPage<PageProps> = (pageProps) => {
   const {
     pageMetaData: { title: pageTitle, description: pageDescription },
   } = pageProps;
-  const lifetimeGithubContributions = 3773;
-  const stackOverflowPoints = 223;
-
-  const iFITDescription = `**Senior Front End Developer • Feb. 21' - Nov. 22' **
-
-  Working closely with designers, back-end developers, product
-  owners and copywriters to build an industry leading wellness &
-  fitness platform that millions of customers see and use. Along
-  with managing React components in tooling like Storybook,
-  multiple Front End repositories & React applications for use as
-  WebViews in hybrid mobile applications we build projects with
-  the latest Front End, Back End & deployment tooling like React
-  Native/Expo, Typescript, NextJs, NX mono repos, TravisCi,
-  Serverless computing with AWS Lambdas etc...
-  
-  Day to day:
-  - Writing reusable UI components using modern React, React Native,
-  build tools, CSS in JS and SCSS.
-  - Building out custom animations in SVG's, SASS & Javascript frameworks &
-  animation api's
-  - Constantly improving quality and performance of front-end codebases.
-  - Ensuring all code works as designed by helping build out an extensive test suite.
-  - Participating in peer code reviews, to produce solid code, collaborate and learn new tech.
-  - Engaging in internal tech talks and other trainings.`;
-  const experientialEverythingDescription = `**Creative Agency Owner • Apr. 17' - Apr. 21' **
-  
-  Agency owner of a distributed digital agency with a niche focus on email design & development services. Responsible for client
-relationships, business development, project management & delivery.
-    
-  The studio was comprised of digital designers & interactive technologists working across the globe. We provided a competitive advantage implementing creative strategies over the long tail of production & brand development.`;
-  const sportsEngineDescription = `**Senior Front End Developer • Mar. 14' - Jan. 21'**
-  
-  Starting as an Interactive Designer, I grew with SportsEngine to become a Senior Front End Developer. Day to day work transformed from a focus on enterprise level client sites, custom API integrations, theme & product enhancements to marketing landing pages, emails, custom JAMStack frontends, Wordpress & Drupal development.
-
-  Day to day:
-  - Developing & maintaining Front-end themes & custom solutions for over 29,000 clients
-  - Technology lead across multiple marketing web properties & business initiatives. 
-  - Creation of Landing pages using Marketo & custom Front Ends
-  - Email Design & Development (Marketo, Salesforce Marketing Cloud/Exact Target)
-  - Conversion of design concepts primarily from InVison, Zeplin, Sketch & Photoshop to standards based HTML, CSS & JS markup.
-  - Execution of web design projects from ideation to deployment
-  - Creation of interactive experiences using JavaScript/jQuery without the help of existing plugins
-  - Theme development & product enhancements for proprietary & open source CMS systems (Drupal, Wordpress)
-  - Coordinating & Managing front end freelance resources
-  - Projecting scope, delivery & project timelines
-  - Regularly using Git & Terminal to push & build application features
-  - Trouble shooting escalated support tickets across CMS systems, custom tools & integrated platforms`;
-  const bestBuyDescription = `**User Experience Designer II • Sep. 13' - Feb. 14'**
-  
-  Worked as a User Experience Designer on the Browse team, with a dedicated focus on user interface from site entry up to
-checkout process.
-
-Responsible for working with project teams to design and deliver high quality client-centered, effective, consistent and usable interfaces.`;
-  const freelanceDescription = `**Owner, Freelance Designer • Jan. 11' - Feb. 14'**
-  
-  Independent Designer & Developer executing projects for businesses in need of interactive websites, identity systems, strategy & design
-  services.
-  
-  Mediums included Interactive Web, Brand Strategy & Identity Systems.`;
-  const imageTrendDescription = `**Implementation and Design Specialist • May 12' - Sep. 13'**
-
-  In house expert on proprietary & open source content management systems, implementation, training & documentation. Day to
-  day task included design & front-end development of client websites using a closed source content management system along
-  with product enhancement & client support.
-
-  Day to day:
-  - Design & Implement client websites using content management systems embedded in ImageTrend products and as stand alone tools
-  - Front-end developer, Strategy, IA, UI, UX - client projects, custom data management & CMS projects
-  - Internal product enhancement
-  - Product & Client Support`;
 
   return (
     <>
       <PageHead pageDescription={pageDescription} pageTitle={pageTitle} />
-      <Section className={styles.aboutHero} sectionType="fullWidthCenter">
-        <MotionFadeInWhenInView className={styles.profileImage}>
-          <FutureImage
-            alt="Ian Roberts – Senior Front End Developer – Work & Resume"
-            height={779}
-            src="/images/ian-roberts-senior-front-end-engineer-work-resume.jpeg"
-            width={1024}
-          />
-        </MotionFadeInWhenInView>
-        <MotionFadeAndStaggerChildrenWhenInView className={`${styles.stats}`}>
-          <div className={`${styles.stat} ${styles.yearsSpentCoding}`}>
-            <span className={styles.number}>{stackOverflowPoints}</span>
-            <span className={styles.description}>StackOverflow points</span>
-          </div>
-          <div className={`${styles.stat} ${styles.numberOfTripsThisYear}`}>
-            <span className={styles.number}>
-              {lifetimeGithubContributions}+
-            </span>
-            <span className={styles.description}>Github contributions</span>
-          </div>
-        </MotionFadeAndStaggerChildrenWhenInView>
-      </Section>
       <Section sectionType="center">
         <Row className={styles.shortDescription} elementType="largeP">
           <MotionFadeInWhenInView>
@@ -175,12 +80,95 @@ Responsible for working with project teams to design and deliver high quality cl
         <Row className={styles.contentRow}>
           <Column className={styles.titleColumn}>
             <MotionFadeInWhenInView>
+              <Title titleType="inlineh3">Designaroni.com</Title>
+            </MotionFadeInWhenInView>
+          </Column>
+          <Column className={styles.contentColumn}>
+            <MotionFadeInWhenInView>
+              <p>
+                <b>
+                  Designer & Developer • Apr. 22{`'`} - present{`'`}
+                </b>
+                <LineBreak />
+                A personal travel, journal, portfolio site built with a Strapi
+                backend headless CMS that serves a GraphQL api, using a
+                Postgresql database & ReactJS/NextJS frontend.
+                <LineBreak />
+                Between the two projects the tech stack uses React, Typescript,
+                NextJS, Postgresql, GraphQL, Sass/CSS modules & framer motion,
+                along with modern front end build tools & automated deployments.
+                <LineBreak />
+                The DevOps pipeline implements automated deployments to test &
+                production environments, the backend deploys to Digital Ocean
+                and the frontend deploys to Vercel.
+                <LineBreak />
+                Serving as a living portfolio of my work, the Strapi backend
+                codebase can be found{" "}
+                <MotionLinkInline
+                  link="https://github.com/designaroni/designaroni-strapi"
+                  text="here"
+                />{" "}
+                , the NextJs frontend can be found{" "}
+                <MotionLinkInline
+                  link="https://github.com/designaroni/designaroni-next"
+                  text="here"
+                />{" "}
+                . More about the site can be found on the{" "}
+                <MotionLinkInline link="/about" text="About" /> page
+              </p>
+            </MotionFadeInWhenInView>
+          </Column>
+        </Row>
+      </Section>
+      <Section>
+        <Row className={styles.contentRow}>
+          <Column className={styles.titleColumn}>
+            <MotionFadeInWhenInView>
               <Title titleType="inlineh3">iFIT</Title>
             </MotionFadeInWhenInView>
           </Column>
           <Column className={styles.contentColumn}>
             <MotionFadeInWhenInView>
-              <Markdown>{iFITDescription}</Markdown>
+              <p>
+                <b>
+                  Senior Front End Developer • Feb. 21{`'`} - Nov. 22{`'`}
+                </b>
+                <LineBreak />
+                Working closely with designers, back-end developers, product
+                owners and copywriters to build an industry leading wellness &
+                fitness platform that millions of customers see and use. Along
+                with managing React components in tooling like Storybook,
+                multiple Front End repositories & React applications for use as
+                WebViews in hybrid mobile applications we build projects with
+                the latest Front End, Back End & deployment tooling like React
+                Native/Expo, Typescript, NextJs, NX mono repos, TravisCi,
+                Serverless computing with AWS Lambdas etc...
+                <LineBreak />
+                Day to day:
+                <ul>
+                  <li>
+                    Writing reusable UI components using modern React, React
+                    Native, build tools, CSS in JS and SCSS.
+                  </li>
+                  <li>
+                    Building out custom animations in SVG{`'`}s, SASS &
+                    Javascript frameworks & animation api{`'`}s
+                  </li>
+                  <li>
+                    Constantly improving quality and performance of front-end
+                    codebases.
+                  </li>
+                  <li>
+                    Ensuring all code works as designed by helping build out an
+                    extensive test suite.
+                  </li>
+                  <li>
+                    Participating in peer code reviews, to produce solid code,
+                    collaborate and learn new tech.
+                  </li>
+                  <li>Engaging in internal tech talks and other trainings.</li>
+                </ul>
+              </p>
             </MotionFadeInWhenInView>
           </Column>
         </Row>
@@ -194,7 +182,21 @@ Responsible for working with project teams to design and deliver high quality cl
           </Column>
           <Column className={styles.contentColumn}>
             <MotionFadeInWhenInView>
-              <Markdown>{experientialEverythingDescription}</Markdown>
+              <p>
+                <b>
+                  Creative Agency Owner • Apr. 17{`'`} - Apr. 21{`'`}
+                </b>
+                <LineBreak />
+                Agency owner of a distributed digital agency with a niche focus
+                on email design & development services. Responsible for client
+                relationships, business development, project management &
+                delivery.
+                <LineBreak />
+                The studio was comprised of digital designers & interactive
+                technologists working across the globe. We provided a
+                competitive advantage implementing creative strategies over the
+                long tail of production & brand development.
+              </p>
             </MotionFadeInWhenInView>
           </Column>
         </Row>
@@ -208,7 +210,65 @@ Responsible for working with project teams to design and deliver high quality cl
           </Column>
           <Column className={styles.contentColumn}>
             <MotionFadeInWhenInView>
-              <Markdown>{sportsEngineDescription}</Markdown>
+              <p>
+                <b>
+                  Senior Front End Developer • Mar. 14{`'`} - Jan. 21{`'`}
+                </b>
+                <LineBreak />
+                Starting as an Interactive Designer, I grew with SportsEngine to
+                become a Senior Front End Developer. Day to day work transformed
+                from a focus on enterprise level client sites, custom API
+                integrations, theme & product enhancements to marketing landing
+                pages, emails, custom JAMStack frontends, Wordpress & Drupal
+                development.
+                <LineBreak />
+                Day to day:
+                <ul>
+                  <li>
+                    Developing & maintaining Front-end themes & custom solutions
+                    for over 29,000 clients.
+                  </li>
+                  <li>
+                    Technology lead across multiple marketing web properties &
+                    business initiatives.
+                  </li>
+                  <li>
+                    Creation of Landing pages using Marketo & custom Front Ends.
+                  </li>
+                  <li>
+                    Email Design & Development (Marketo, Salesforce Marketing
+                    Cloud/Exact Target).
+                  </li>
+                  <li>
+                    Conversion of design concepts primarily from InVison,
+                    Zeplin, Sketch & Photoshop to standards based HTML, CSS & JS
+                    markup.
+                  </li>
+                  <li>
+                    Execution of web design projects from ideation to deployment
+                  </li>
+                  <li>
+                    Creation of interactive experiences using JavaScript/jQuery
+                    without the help of existing plugins.
+                  </li>
+                  <li>
+                    Theme development & product enhancements for proprietary &
+                    open source CMS systems (Drupal, Wordpress).
+                  </li>
+                  <li>
+                    Coordinating & Managing front end freelance resources.
+                  </li>
+                  <li>Projecting scope, delivery & project timelines.</li>
+                  <li>
+                    Regularly using Git & Terminal to push & build application
+                    features.
+                  </li>
+                  <li>
+                    Trouble shooting escalated support tickets across CMS
+                    systems, custom tools & integrated platforms.
+                  </li>
+                </ul>
+              </p>
             </MotionFadeInWhenInView>
           </Column>
         </Row>
@@ -222,7 +282,18 @@ Responsible for working with project teams to design and deliver high quality cl
           </Column>
           <Column className={styles.contentColumn}>
             <MotionFadeInWhenInView>
-              <Markdown>{bestBuyDescription}</Markdown>
+              <p>
+                <b>
+                  User Experience Designer II • Sep. 13{`'`} - Feb. 14{`'`}
+                </b>
+                Worked as a User Experience Designer on the Browse team, with a
+                dedicated focus on user interface from site entry up to checkout
+                process.
+                <LineBreak />
+                Responsible for working with project teams to design and deliver
+                high quality client-centered, effective, consistent and usable
+                interfaces.
+              </p>
             </MotionFadeInWhenInView>
           </Column>
         </Row>
@@ -236,7 +307,18 @@ Responsible for working with project teams to design and deliver high quality cl
           </Column>
           <Column className={styles.contentColumn}>
             <MotionFadeInWhenInView>
-              <Markdown>{freelanceDescription}</Markdown>
+              <p>
+                <b>
+                  Owner, Freelance Designer • Jan. 11{`'`} - Feb. 14{`'`}
+                </b>
+                <LineBreak />
+                Independent Designer & Developer executing projects for
+                businesses in need of interactive websites, identity systems,
+                strategy & design services.
+                <LineBreak />
+                Mediums included Interactive Web, Brand Strategy & Identity
+                Systems.
+              </p>
             </MotionFadeInWhenInView>
           </Column>
         </Row>
@@ -250,7 +332,33 @@ Responsible for working with project teams to design and deliver high quality cl
           </Column>
           <Column className={styles.contentColumn}>
             <MotionFadeInWhenInView>
-              <Markdown>{imageTrendDescription}</Markdown>
+              <p>
+                <b>
+                  Implementation and Design Specialist • May 12{`'`} - Sep. 13
+                  {`'`}
+                </b>
+                <LineBreak />
+                In house expert on proprietary & open source content management
+                systems, implementation, training & documentation. Day to day
+                task included design & front-end development of client websites
+                using a closed source content management system along with
+                product enhancement & client support.
+                <LineBreak />
+                Day to day:
+                <ul>
+                  <li>
+                    Design & Implement client websites using content management
+                    systems embedded in ImageTrend products and as stand alone
+                    tools.
+                  </li>
+                  <li>
+                    Front-end developer, Strategy, IA, UI, UX - client projects,
+                    custom data management & CMS projects.
+                  </li>
+                  <li>Internal product enhancement.</li>
+                  <li>Product & Client Support.</li>
+                </ul>
+              </p>
             </MotionFadeInWhenInView>
           </Column>
         </Row>
